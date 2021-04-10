761a64fff183a1705eb95d23f4f7659c6b6e21c9
lofi4 <82137656+lofi4@users.noreply.github.com>
2021-04-10 12:38:08 +0900

    botton fix2

--- sema.js
+++ sema.js
@@ -25,10 +25,14 @@ buttonClick('fifth','five')
 buttonClick('sixth','six')
 
 
+
+
 function goBack() {
     let backButton = document.getElementById('goback')
     backButton.addEventListener('click',
-    window.history.back())
+    function () {window.history.back()})
   }
+
+
 
   goBack()
